2525c47457acb005e1ccacc8986715856ca3ee41
Valeria-Apostolidi <106014953+Valeria-Apostolidi@users.noreply.github.com>
2022-09-27 22:21:14 +0300

    task-1 correction

--- js/task-01.js
+++ js/task-01.js
@@ -1,9 +1,9 @@
 const categories = document.querySelectorAll(".item");
 console.log("Number of categories:", categories.length);
 
-for (const category of categories) {
+const category = [...categories].map((category) => {
   const child = category.firstElementChild;
   const lastChild = category.lastElementChild;
   console.log("Category:", child.textContent);
   console.log("Elements:", lastChild.children.length);
-}
+});
