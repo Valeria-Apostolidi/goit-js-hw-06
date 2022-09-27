6070eef0454f764793fb2d1d0ddbf4a5b2e279dd
Valeria-Apostolidi <106014953+Valeria-Apostolidi@users.noreply.github.com>
2022-09-27 22:18:39 +0300

    Task-2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,23 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const list = document.querySelector("#ingredients");
+
+const newIngredients = ingredients.map((ingredient) => {
+  const li = document.createElement("li");
+  li.textContent = ingredient;
+  li.classList.add("item");
+  list.append(li);
+});
+
+// const newIngredient = ingredients.map(
+//   (ingredient) => `<li class = item>${ingredient}</li>`
+// );
+// list.insertAdjacentHTML("beforeend", newIngredient);
+// console.log(newIngredient);
