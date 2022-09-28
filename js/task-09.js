b96e2bc50853aaf9bd690ab2318290abf154e707
Valeria-Apostolidi <106014953+Valeria-Apostolidi@users.noreply.github.com>
2022-09-28 23:16:19 +0300

    Task-9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColorButton = document.querySelector(".change-color");
+const body = document.querySelector("body");
+const span = document.querySelector(".color");
+
+changeColorButton.addEventListener("click", onClick);
+
+function onClick(event) {
+  body.style.backgroundColor = getRandomHexColor();
+  span.textContent = getRandomHexColor();
+}
