ddd0c9c5e9cf39c9e0bb1cae36fc222008fc6f4c
Valeria-Apostolidi <106014953+Valeria-Apostolidi@users.noreply.github.com>
2022-09-28 23:05:55 +0300

    Task-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+  const { email, password } = event.currentTarget.elements;
+
+  const data = {
+    email: email.value,
+    password: password.value,
+  };
+
+  if (email.value === "" || password.value === "") {
+    alert("Please, fill all fields!");
+  }
+
+  console.log(data);
+  event.currentTarget.reset();
+}
