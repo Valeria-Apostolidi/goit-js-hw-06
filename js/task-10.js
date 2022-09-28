25add2eb88d99f60c1a5a1b7577282ff719d5844
Valeria-Apostolidi <106014953+Valeria-Apostolidi@users.noreply.github.com>
2022-09-29 00:29:10 +0300

    Task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,32 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const input = document.querySelector("input");
+const createBtn = document.querySelector("button[data-create]");
+const destroyBtn = document.querySelector("button[data-destroy]");
+const mainDiv = document.querySelector("#boxes");
+
+createBtn.addEventListener("click", createBoxes);
+
+let value = 3;
+
+function createBoxes() {
+  for (let i = 1; i <= input.value; i += 1) {
+    value += 1;
+    const div = document.createElement("div");
+    div.classList.add("box");
+    div.style.width = value * 10 + "px";
+    div.style.height = value * 10 + "px";
+    div.style.backgroundColor = getRandomHexColor();
+    mainDiv.append(div);
+
+    console.log(div);
+  }
+}
+
+destroyBtn.addEventListener("click", () => {
+  const div = document.querySelector(".box");
+  div.remove();
+  console.log(mainDiv);
+});
